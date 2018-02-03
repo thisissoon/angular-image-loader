228276b9c4eee0a82828606fc1709cda9dcefa2e
Ed Opare-Aryee <ed@thisissoon.com>
2018-02-03 23:56:20 +0000

    fix(ImageLoader): fixing imports so all symbols can be imported from @thisissoon/angular-image-loade

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 export { ImageLoaderModule } from './src/app/image-loader/image-loader.module';
 export { ImageLoaderComponent } from './src/app/image-loader/image-loader/image-loader.component';
-import { loadedClass, notLoadedClass } from './src/app/image-loader/shared/classes';
-import { eventResize } from './src/app/image-loader/shared/events';
-import { ResponsiveImage, RetinaImage, Size } from './src/app/image-loader/shared/image.model';
-import { Breakpoint } from './src/app/image-loader/shared/breakpoint.model';
+export { loadedClass as loadedClassImg, notLoadedClass as notLoadedClassImg } from './src/app/image-loader/shared/classes';
+export { eventResize } from './src/app/image-loader/shared/events';
+export { ResponsiveImage, RetinaImage, Size } from './src/app/image-loader/shared/image.model';
+export { Breakpoint } from './src/app/image-loader/shared/breakpoint.model';
 
 export { VideoLoaderComponent } from './src/app/video-loader/video-loader/video-loader.component';
 export { VideoLoaderModule } from './src/app/video-loader/video-loader.module';
-export { loadedClass, notLoadedClass } from './src/app/video-loader/shared/classes';
+export { loadedClass as loadedClassVideo, notLoadedClass as notLoadedClassVideo } from './src/app/video-loader/shared/classes';
 export { ResponsiveVideo, Video } from './src/app/video-loader/shared/video.model';
